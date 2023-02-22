a996ddac0ac8fb88c7eb795b8149f3f890c94af4
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-02-22 22:09:02 +0300

    thunder git sync added

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,18 +1,17 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
-
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
-router.register('owners', OwnerViewSet)
-router.register(r'mycats', LightCatViewSet)
+router.register("cats", CatViewSet)
+router.register("owners", OwnerViewSet)
+router.register(r"mycats", LightCatViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path("", include(router.urls)),
+    path("api-token-auth/", views.obtain_auth_token),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
